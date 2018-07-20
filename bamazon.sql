47051ad5687e8ced9907c73e8c0746350112d386
Avickerman <Avickerm@gmail.com>
2018-07-20 00:26:23 -0500

    push

---
+++ bamazon.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Bamazon;
+USE Bamazon;
+
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Minnesota Wild Jersey", "Jersey", 170, 600),
+		("Washington Capitals Jersey", "Jersey", 180, 1000),
+        ("Vegas Knights Jersey", "Jersey", 170, 700),
+        ("New York Rangers Jersey", "Jersey", 170, 500),
+        ("Minnesota North Stars Jersey", "Jersey", 200, 300),
+        ("Minnesota NS Hat", "Hats", 50, 45),
+        ("Bauer Hat", "Hat", 25, 100),
+        ("CCM Hat", "Hat", 25, 100),
+        ("Maple Leafs Hat", "Hat", 30, 60),
+        ("Toronto Maple Leafs Jersey", "Jersey", 170, 700);
+
+
+
+
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,120 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "MAMP",
+
+
+    port: 3307,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: " ",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+
+function start() {
+    // Prompt the user to select an item
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the Item ID which you would like to purchase.',
+            validate: validateInput,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many do you need?',
+            validate: validateInput,
+            filter: Number
+        }
+    ]).then(function (input) {
+
+
+        var item = input.item_id;
+        var quantity = input.quantity;
+
+        var queryStr = 'SELECT * FROM products WHERE ?';
+
+        connection.query(queryStr, { item_id: item }, function (err, data) {
+            if (err) throw err;
+
+            if (data.length === 0) {
+                console.log('ERROR: Invalid Item ID.');
+                displayInventory();
+
+            } else {
+                var productData = data[0];
+
+                if (quantity <= productData.stock_quantity) {
+                    console.log('The product you requested is in stock! Placing your order....');
+
+                    // Construct the updating query string
+                    var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+                    // Update the inventory
+                    connection.query(updateQueryStr, function (err, data) {
+                        if (err) throw err;
+                        // console log total price
+                        console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+
+                        connection.end();
+                    })
+                } else {
+                    console.log('Product amount not in stock, order not placed!');
+                    console.log('You need to modify your order.');
+                    console.log("\n------------------------------------------\n");
+
+                    displayInventory();
+                }
+            }
+        })
+    })
+}
+
+function displayInventory() {
+
+    queryStr = 'SELECT * FROM products';
+
+    connection.query(queryStr, function (err, data) {
+        if (err) throw err;
+
+        console.log('Existing Inventory: ');
+
+
+        var strOut = '';
+        for (var i = 0; i < data.length; i++) {
+            strOut = '';
+            strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+            strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+            strOut += 'Department: ' + data[i].department_name + '  //  ';
+            strOut += 'Price: $' + data[i].price + '\n';
+
+            console.log(strOut);
+        }
+
+        console.log("-----------------------\n");
+
+        //Prompt to purchase
+        promptUserPurchase();
+    })
+}
+
+function runBamazon() {
+
+    displayInventory();
+}
+
+runBamazon();
+
