af93404dc089761bbd7b8af6a549cc67fe6e2ee7
Avickerman <Avickerm@gmail.com>
2018-07-20 00:31:40 -0500

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "MAMP",
+    host: "localserver",
 
 
-    port: 3307,
+    port: 3306,
 
     // Your username
     user: "root",
